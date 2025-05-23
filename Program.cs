66bcd2b5ec4f929e86037b02f5453bb71ff827ec
HarryNg59 <huyheo05092004@gmail.com>
2025-05-24 00:33:54 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entity -> Database, Table
+            // Database - SQL Server: data01 -> DbContext
+            // --product
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
